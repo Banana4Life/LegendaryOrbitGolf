557d0f21d37d31cf5405884ccef476f5308a2bef
Phillip Schichtel <phillip@schich.tel>
2020-10-03 19:09:14 +0200

    camera movement and stars and stuff

---
+++ Assets/Objects/Background/TextureScroller.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Objects.Background
+{
+	[RequireComponent(typeof(Renderer))]
+	public class TextureScroller : MonoBehaviour
+	{
+		public float scrollSpeed = 1;
+		public Camera playerCamera;
+		private Renderer _renderer;
+		private static readonly int MainTex = Shader.PropertyToID("_MainTex");
+		private Vector2 _lastCameraPos;
+
+		private void Start()
+		{
+			_renderer = GetComponent<Renderer>();
+			_lastCameraPos = CameraPosition();
+		}
+
+		private Vector2 CameraPosition()
+		{
+			var pos = playerCamera.transform.position;
+			return new Vector2(pos.x, pos.z);
+		}
+
+		private Vector2 CameraDelta()
+		{
+			var newPos = CameraPosition();
+			var delta = newPos - _lastCameraPos;
+			_lastCameraPos = newPos;
+			return delta;
+		}
+
+		void Update ()
+		{
+			var mat = _renderer.material;
+			var currentOffset = mat.GetTextureOffset(MainTex);
+			var newOffset = CameraDelta() * (Time.deltaTime * scrollSpeed);
+			mat.SetTextureOffset(MainTex, currentOffset - newOffset);
+			var localScale = transform.localScale;
+			
+			mat.SetTextureScale(MainTex, new Vector2(localScale.x, localScale.z));
+		}
+	}
+}
---
+++ Assets/Objects/Player/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+namespace Objects.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float speed = 80;
+        public float zoomSpeed = 10;
+        public float minZoom = 0;
+        public float maxZoom = 100;
+        public float startZoom = 0.5f;
+
+        public float borderSize = 30;
+
+        public Camera playerCamera;
+        private Transform _cameraTransform;
+
+        public GameObject background;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+            _cameraTransform = playerCamera.transform;
+            var pos = _cameraTransform.position;
+        
+            _cameraTransform.rotation = Quaternion.AngleAxis(90, Vector3.right);
+            _cameraTransform.position = new Vector3(0, Mathf.Lerp(minZoom, maxZoom, startZoom), 0);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var mousePos = Input.mousePosition;
+
+            float x = 0;
+            float z = 0;
+        
+            if (mousePos.x >= 0 && mousePos.x < borderSize || Input.GetKey(KeyCode.A))
+            {
+                x = -1;
+            }
+            if (mousePos.x < Screen.width && mousePos.x >= Screen.width - borderSize || Input.GetKey(KeyCode.D))
+            {
+                x = 1;
+            }
+
+            if (mousePos.y >= 0 && mousePos.y < borderSize || Input.GetKey(KeyCode.S))
+            {
+                z = -1;
+            }
+
+            if (mousePos.y < Screen.height && mousePos.y >= Screen.height - borderSize || Input.GetKey(KeyCode.W))
+            {
+                z = 1;
+            }
+
+            transform.Translate(new Vector3(x, 0, z).normalized * (speed * Time.deltaTime));
+
+            var currentCameraHeight = _cameraTransform.localPosition.y;
+            var newCameraHeight = Mathf.Clamp( currentCameraHeight - (Input.mouseScrollDelta.y * zoomSpeed), minZoom, maxZoom);
+            _cameraTransform.localPosition = new Vector3(0, newCameraHeight, 0);
+        }
+    }
+}
