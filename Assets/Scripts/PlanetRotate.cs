0315ae234b71416d068fe37ec3d6292043a9dee7
Alex <alex.pollich@t-online.de>
2020-10-04 13:56:32 +0200

    rotate planet

---
+++ Assets/Scripts/PlanetRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotate : MonoBehaviour
+{
+    public float rotationSpeed = 20.0f;
+    private Vector3 _axis;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _axis = Random.insideUnitSphere;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, _axis, rotationSpeed * Time.deltaTime);
+    }
+}
