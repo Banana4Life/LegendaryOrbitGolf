3437aa7c3af0e405718518cc41ebd2439529fed0
Alex <alex.pollich@t-online.de>
2020-10-06 01:40:31 +0200

    change hud text alignment

--- Assets/Scripts/Hud.cs
+++ Assets/Scripts/Hud.cs
@@ -27,6 +27,6 @@ public class Hud : MonoBehaviour
     
     private void UpdateText()
     {
-        shotCountText.text = $"Shot: {shotCount} / {shotsForPar} for Par";
+        shotCountText.text = $"Shot / Par \n {shotCount} / {shotsForPar}";
     }
 }
