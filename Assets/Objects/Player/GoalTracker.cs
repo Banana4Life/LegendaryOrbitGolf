6066be2f353dba5291850fee0368bd8422080d63
Phillip Schichtel <phillip@schich.tel>
2020-10-05 21:22:05 +0200

    show direction towards goal

---
+++ Assets/Objects/Player/GoalTracker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Objects.Player
+{
+    public class GoalTracker : MonoBehaviour
+    {
+        public World world;
+
+        private float angle = 0;
+        // Update is called once per frame
+        void Update()
+        {
+            var pos = transform.position;
+            var goalPosition = world.goalPlanet.transform.position;
+            var dir = goalPosition - pos;
+            Debug.Log(Mathf.Atan2(dir.z, dir.x));
+            transform.rotation = Quaternion.AngleAxis((Mathf.Atan2(dir.x, dir.z) + Mathf.PI) * Mathf.Rad2Deg, Vector3.up);
+        }
+    }
+}
